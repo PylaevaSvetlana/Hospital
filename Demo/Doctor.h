832aafe7c10b404fd0398d7dac60d052dc3ad988
PylaevaSvetlana <pylaeva.sweta@yandex.ru>
2024-11-28 21:07:22 +0300

    Больница

--- Demo/Doctor.h
+++ Demo/Doctor.h
@@ -13,6 +13,7 @@ namespace Hospital
 		std::string specialization;
 	public:
 		Doctor(const std::string& FirstName, std::string& SecondName, std::string& Patronymic, std::string& specialization);
+		static std::shared_ptr<Doctor> CreateDoctor(const std::string& FirstName, std::string& SecondName, std::string& Patronymic, std::string& specialization);
 	};
 
 }
