42e02901f2498d94acdf5a81b0fcf94a420148c0
PylaevaSvetlana <pylaeva.sweta@yandex.ru>
2024-11-28 23:40:57 +0300

    Класс Доктор

--- Demo/Doctor.cpp
+++ Demo/Doctor.cpp
@@ -1,3 +1,46 @@
 #include "doctor.h"
+#include <sstream>
+using namespace Hospital;
+Hospital::Doctor::Doctor(const std::string& firstName, const std::string& secondName, const std::string& patronymicName, const std::string& specialization) :firstName{ firstName }, secondName{ secondName }, patronymicName{ patronymicName }, specialization{ specialization } {}
 
-Hospital::Doctor::Doctor(const std::string& FirstName, std::string& SecondName, std::string& Patronymic, std::string& specialization) :FirstName{ FirstName }, SecondName{ SecondName }, Patronymic{ Patronymic }, specialization{ specialization } {}
+std::shared_ptr<Doctor> Hospital::Doctor::CreateDoctor(const std::string& firstName, const std::string& secondName, const std::string& patronymicName, const std::string& specialization)
+{
+	return std::make_shared<Doctor>(Doctor{ firstName, secondName, patronymicName, specialization });
+}
+
+
+bool Hospital::Doctor::AddPatient(std::shared_ptr<Patient>& patient)
+{
+	this->patients.push_back(patient);
+	patient->GetPatients().push_back(shared_from_this());
+	return true;
+}
+
+std::string Hospital::Doctor::ToString() const
+{
+	std::stringstream buffer{};
+	buffer << GetFirstName() << " " << GetSecondName() << " " << GetPatronymicName() << " " << GetSpecialization();
+	return buffer.str();
+}
+
+std::string Hospital::Doctor::GetFirstName()
+{
+	return firstName;
+}
+
+std::string Hospital::Doctor::GetSecondName()
+{
+	return secondName;
+}
+
+std::string Hospital::Doctor::GetPatronymicName()
+{
+	return patronymicName;
+}
+
+std::string Hospital::Doctor::GetSpecialization()
+{
+	return specialization;
+}
+
+
--- Demo/Doctor.h
+++ Demo/Doctor.h
@@ -4,16 +4,24 @@
 
 namespace Hospital
 {
-	class Doctor final
+	class Doctor;
+	class Patient;
+	class Doctor final : public std::enable_shared_from_this<Doctor>
 	{
 	private:
-		std::string FirstName;
-		std::string SecondName;
-		std::string Patronymic;
+		std::string firstName;
+		std::string secondName;
+		std::string patronymicName;
 		std::string specialization;
 	public:
-		Doctor(const std::string& FirstName, std::string& SecondName, std::string& Patronymic, std::string& specialization);
-		static std::shared_ptr<Doctor> CreateDoctor(const std::string& FirstName, std::string& SecondName, std::string& Patronymic, std::string& specialization);
+		Doctor(const std::string& firstName, const std::string& secondName, const std::string& patronymicName, const std::string& specialization);
+		static std::shared_ptr<Doctor> CreateDoctor(const std::string& firstName, const std::string& secondName, const std::string& patronymicName, const std::string& specialization);
+		bool AddPatient(std::shared_ptr<Patient>& patient);
+		std::string ToString() const;
+		std::string GetFirstName();
+		std::string GetSecondName();
+		std::string GetPatronymicName();
+		std::string GetSpecialization();
 	};
 
 }
