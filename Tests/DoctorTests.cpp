9b38806439e06c18a5e19f017e01f07601ab5265
PylaevaSvetlana <pylaeva.sweta@yandex.ru>
2024-12-13 11:01:19 +0300

    Больница

--- Tests/DoctorTests.cpp
+++ Tests/DoctorTests.cpp
@@ -10,8 +10,37 @@ namespace Tests
 	{
 	public:
 
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Ctor_ValidData_Success)
 		{
+			const std::string firstName{ "Иван" };
+			const std::string secondName{ "Иванов" };
+			const std::string patronymicName{ "Ивановиич" };
+			const std::string specialization{ "Доктор" };
+
+			auto  doctor = Doctor::CreateDoctor(firstName, secondName, patronymicName, specialization);
+			
+			Assert::AreEqual(doctor->GetFirstName(), firstName);
+			Assert::AreEqual(doctor->GetSecondName(), secondName);
+			Assert::AreEqual(doctor->GetPatronymicName(), patronymicName);
+			Assert::AreEqual(doctor->GetSpecialization(), specialization);
+		}
+
+		TEST_METHOD(AddPatient_ValidData_True)
+		{
+			const std::string firstName1{ "Иван" };
+			const std::string secondName1{ "Иванов" };
+			const std::string patronymicName1{ "Ивановиич" };
+			const std::string specialization{ "Доктор" };
+			const std::string firstName2{ "Пётр" };
+			const std::string secondName2{ "Петров" };
+			const std::string patronymicName2{ "Петрович" };
+			const std::string disease{ "Болезнь" };
+			auto  doctor = Doctor::CreateDoctor(firstName1, secondName1, patronymicName1, specialization);
+			auto  patient = Patient::CreatePatient(firstName2, secondName2, patronymicName2, disease);
+
+			auto result = doctor->AddPatient(patient);
+
+			Assert::IsTrue(result);
 		}
 	};
 }
